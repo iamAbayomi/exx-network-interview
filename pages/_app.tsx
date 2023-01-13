f01a8d97887191052c56cec5a6fc4786e8f6dcba
iamAbayomi <oladinifolawiyo@gmail.com>
2023-01-13 08:32:48 +0100

    feat: installed the required fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        cross-origin={true}
+        cross-origin={"true"}
       />
       <link
         href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        cross-origin={true}
+        cross-origin={"true"}
       />
       <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
